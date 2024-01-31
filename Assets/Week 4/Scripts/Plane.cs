b7f1b49f1886b1316203505f4d99457a983b2042
pocockm <pocockm@OA-S141-26>
2024-01-31 14:39:05 -0500

    plane moving along flight path

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Plane : MonoBehaviour
@@ -8,12 +9,45 @@ public class Plane : MonoBehaviour
     public float newPointThreshold = 0.2f;
     Vector2 lastPosition;
     LineRenderer lineRenderer;
+    Vector2 currentPosition;
+    Rigidbody2D rb;
+    public float speed = 1;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if (points.Count > 0)
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rb.rotation = -angle;
+        }
+        rb.MovePosition(rb.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        if (points.Count > 0)
+        {
+            if (Vector2.Distance(currentPosition, points[0]) < newPointThreshold)
+            {
+                points.RemoveAt(0);
+
+                for(int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+                lineRenderer.positionCount--;
+            }
+        }
     }
 
     private void OnMouseDown()
