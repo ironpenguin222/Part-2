9b858f6e6da6b9dedefeb08e8d46bd08ba317503
pocockm <pocockm@OA-S141-26>
2024-02-14 14:38:15 -0500

    Task 2 Done

---
+++ Assets/Week 6/Scripts/Scene Change Thing.cs
@@ -0,0 +1,25 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeThing : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextScene();
+        }
+    }
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    
+}
